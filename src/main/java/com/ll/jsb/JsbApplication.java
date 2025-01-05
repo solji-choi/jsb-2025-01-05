ed9ccac6c738444be7ab59bd3fe01a2ab0c3991e
solji <solji.choi89@gmail.com>
2025-01-05 17:17:55 +0900

    setting

---
+++ src/main/java/com/ll/jsb/JsbApplication.java
@@ -0,0 +1,13 @@
+package com.ll.jsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsbApplication.class, args);
+	}
+
+}
